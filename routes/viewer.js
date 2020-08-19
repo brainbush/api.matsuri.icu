adf23f2d18a9bfec1b36f9dbf2deeaa72111efbb
brainbush <960821@gmail.com>
2020-08-19 16:52:14 +0800

    错了错了，aggregate真相（指node吃内存太凶了）

--- routes/viewer.js
+++ routes/viewer.js
@@ -12,17 +12,9 @@ function start_time_compare(a, b) {
     return 0;
 }
 
-function time_compare(a, b) {
-    if (a.time < b.time)
-        return -1;
-    if (a.time > b.time)
-        return 1;
-    return 0;
-}
-
 function check_origin(origin) {
     if (!origin) return false
-    return origin.substring('matsuri.icu') !== -1;
+    return origin.includes('matsuri.icu');
 }
 
 router.get('/:mid', async (req, res) => {
@@ -46,23 +38,50 @@ router.get('/:mid', async (req, res) => {
             comments = await db.collection('clip').aggregate(
                 [
                     {$match: {"full_comments.user_id": mid}},
-                    {$unwind: "$full_comments"},
-                    {$match: {"full_comments.user_id": mid}},
-                    {$group: {_id: "$id", full_comments: {$addToSet: "$full_comments"}}}
+                    {
+                        $lookup: {
+                            from: "channel",
+                            localField: "bilibili_uid",
+                            foreignField: "bilibili_uid",
+                            as: "channel_info"
+                        }
+                    },
+                    {
+                        $project: {
+                            _id: 0,
+                            "id": 1,
+                            "bilibili_uid": 1,
+                            "start_time": 1,
+                            "title": 1,
+                            "cover": 1,
+                            "danmu_density": 1,
+                            "end_time": 1,
+                            "total_danmu": 1,
+                            "total_gift": 1,
+                            "total_reward": 1,
+                            "total_superchat": 1,
+                            "views": 1,
+                            channel_info: {
+                                $arrayElemAt: ["$channel_info", 0]
+                            },
+                            full_comments: {
+                                $filter: {
+                                    input: "$full_comments",
+                                    as: "comment",
+                                    cond: {$eq: ["$$comment.user_id", mid]}
+                                }
+                            }
+                        }
+                    }
                 ]).toArray();
             for (const clip of comments) {
                 let k = {};
-                let clip_info;
-                clip_info = await db.collection('clip').findOne({id: clip._id}, {
-                    projection: {_id: 0, full_comments: 0, highlights: 0}
-                });
-                let channel_info = await db.collection('channel').findOne({bilibili_uid: clip_info.bilibili_uid}, {projection: {name: 1}});
-                if (channel_info) {
-                    k.clip_info = clip_info;
-                    k.clip_info.name = channel_info.name;
-                    k.full_comments = clip.full_comments.sort(time_compare);
-                    final_list.push(k)
-                }
+                clip.name = clip.channel_info.name;
+                delete clip.channel_info
+                k.full_comments = clip.full_comments;
+                delete clip.full_comments
+                k.clip_info = clip
+                final_list.push(k)
             }
             final_list.sort(start_time_compare)
             if (final_list.length > 0) {
