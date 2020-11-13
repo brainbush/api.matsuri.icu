6054ff711e632d752bd248ad2164fedb7804e0f6
brainbush <960821@gmail.com>
2020-11-13 10:53:41 +0800

    上Sentry！

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const logger = require('morgan');
 const MongoClient = require('mongodb').MongoClient;
 const redis = require('async-redis');
 
+const Sentry = require('@sentry/node');
+const Tracing = require('@sentry/tracing');
+
 const indexRouter = require('./routes/index');
 const channelRouter = require('./routes/channel');
 const clipRouter = require('./routes/clip');
@@ -11,19 +14,41 @@ const viewerRouter = require('./routes/viewer');
 const offCommentsRouter = require('./routes/off_comments');
 
 const mongo_url = 'mongodb://localhost:27017/';
-const mongo_options = {useUnifiedTopology: true, auto_reconnect: true, poolSize: 10};
+const mongo_options = {
+    useUnifiedTopology: true,
+    auto_reconnect: true,
+    poolSize: 20,
+    connectTimeoutMS: 60000,
+    serverSelectionTimeoutMS: 5000
+};
 const db_name = 'matsuri_icu';
 
 const app = express();
+
+Sentry.init({
+    dsn: process.env.SentryDSN,
+    integrations: [
+        new Sentry.Integrations.Http({tracing: true}),
+        new Tracing.Integrations.Express({app}),
+    ],
+    tracesSampleRate: 1.0,
+});
+
+app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.tracingHandler());
+app.use(Sentry.Handlers.errorHandler());
+
 MongoClient.connect(mongo_url, mongo_options, (err, client) => {
         if (err) throw err;
-        console.log('DB connected, Authorization is:' + process.env.Authorization);
+        console.log(`DB connected, Authorization is:${process.env.Authorization}, Sentry dsn: ${process.env.SentryDSN}`);
         app.locals.db = client.db(db_name);
     }
-);
+).then(() => {
+});
 app.locals.redis_client = redis.createClient();
 
-app.use(logger('short'));
+// noinspection JSCheckFunctionSignatures
+app.use(logger("short"));
 app.use(express.json({limit: '500mb'}));
 app.use(express.urlencoded({extended: false}));
 
